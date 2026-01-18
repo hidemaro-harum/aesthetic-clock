5686b12dbdccf7850cb74eb984db8231e4a5a674
Hidemaro Endo <malouch3498@gmail.com>
2026-01-18 11:18:27 +0900

    update config files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // または GitHub リポジトリ名に合わせて '/aesthetic-clock/'
+  base: '/aesthetic-clock/',
 })
